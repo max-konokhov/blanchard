4e88abf4193a1801b436db92ef6b6d4de53e5861
max-konokhov <makc.kohoxob@gmail.com>
2022-02-02 16:28:11 +0300

    добавил набросок секции события

---
+++ js/events.js
@@ -0,0 +1,50 @@
+(() => {
+
+    let eventsSlider = new Swiper(".events__swiper", {
+        speed: 400,
+        slidesPerView: 3,
+        spaceBetween: 50,
+        
+        // pagination: {
+        //   el: ".g-swiper__pagination",
+        //   type: "fraction"
+        // },
+        navigation: {
+          nextEl: ".events__btn-nav--next",
+          prevEl: ".events__btn-nav--prev",
+          // disabledClass: ".g-swiper__btn-nav--disabled",
+          // disabledClass: ".green"
+        },
+
+
+        a11y: false,
+        keyboard: true, // можно управлять с клавиатуры стрелками влево/вправо
+        
+        // Дальнейшие надстройки делают слайды вне области видимости не фокусируемыми 
+        watchSlidesProgress: true,
+        watchSlidesVisibility: true,
+        slideVisibleClass: 'slide-visible',
+        
+        on: {
+          init: function () {
+            this.slides.forEach(slide => {
+              if (!slide.classList.contains('slide-visible')) {
+                slide.tabIndex = '-1';
+              } else {
+                slide.tabIndex = '';
+              }
+            });
+          },
+          slideChange: function () {
+            this.slides.forEach(slide => {
+              if (!slide.classList.contains('slide-visible')) {
+                slide.tabIndex = '-1';
+              } else {
+                slide.tabIndex = '';
+              }
+            });
+          }
+        }
+    });
+
+})();
