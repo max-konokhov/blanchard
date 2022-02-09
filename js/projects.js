86b094d7b412294245c14757b06a788f8ee6377e
max-konokhov <makc.kohoxob@gmail.com>
2022-02-09 17:33:32 +0300

    добавил набросок контактов, фикс для проектов

--- js/projects.js
+++ js/projects.js
@@ -51,34 +51,7 @@
         },
 
 
-        // a11y: false,
-        // keyboard: true, // можно управлять с клавиатуры стрелками влево/вправо
         
-        // // Дальнейшие надстройки делают слайды вне области видимости не фокусируемыми 
-        // watchSlidesProgress: true,
-        // watchSlidesVisibility: true,
-        // slideVisibleClass: 'slide-visible',
-        
-        // on: {
-        //   init: function () {
-        //     this.slides.forEach(slide => {
-        //       if (!slide.classList.contains('slide-visible')) {
-        //         slide.tabIndex = '-1';
-        //       } else {
-        //         slide.tabIndex = '';
-        //       }
-        //     });
-        //   },
-        //   slideChange: function () {
-        //     this.slides.forEach(slide => {
-        //       if (!slide.classList.contains('slide-visible')) {
-        //         slide.tabIndex = '-1';
-        //       } else {
-        //         slide.tabIndex = '';
-        //       }
-        //     });
-        //   }
-        // }
     });
 
 
