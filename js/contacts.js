1bc5d7a3a11374570c80913b08b2987510ff93b9
max-konokhov <makc.kohoxob@gmail.com>
2022-02-11 13:25:00 +0300

    добавил карту

--- js/contacts.js
+++ js/contacts.js
@@ -41,6 +41,53 @@
     });
 
 
+    //Яндекс-карта с кастомной меткой
+
+    // Функция ymaps.ready() будет вызвана, когда
+    // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+    ymaps.ready(init);
+    function init() {
+        // Создание карты.
+        const mapElem = document.querySelector('#map');
+        const myMap = new ymaps.Map(
+            "map",
+                { 
+                center: [55.760223, 37.614741],  
+                zoom: 14, 
+                controls: ['geolocationControl', 'zoomControl']
+                },
+                { 
+                suppressMapOpenBlock: true,
+                geolocationControlSize: "large",
+                geolocationControlPosition:  { top: "200px", right: "20px" },
+                geolocationControlFloat: 'none',
+                zoomControlSize: "small",
+                zoomControlFloat: "none",
+                zoomControlPosition: { top: "120px", right: "20px" }
+                }
+        );
+
+        myMap.behaviors.disable('scrollZoom');
+
+        // Создание геообъекта с типом точка (метка).
+        const myPlacemark = new ymaps.Placemark(
+            [55.760223, 37.614741],
+            {},
+            {
+                iconLayout: "default#image",
+                iconImageHref: 'img/contacts/map-marker.svg',
+                iconImageSize: [20, 20],
+                iconImageOffset: [-10, -10],
+            }
+        );
+
+    myMap.geoObjects.add(myPlacemark);
+
+    setTimeout(() => {
+        myMap.container.fitToViewport();
+    }, 5000);
+    }
+
 
 
 
